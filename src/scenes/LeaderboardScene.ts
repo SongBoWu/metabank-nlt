71c707bc8caf4f6458f2ac820be715a2bcf93302
SongBoWu <hv46328@gmail.com>
2022-12-16 00:18:53 +0100

    Show records in leaderboard

--- src/scenes/LeaderboardScene.ts
+++ src/scenes/LeaderboardScene.ts
@@ -5,6 +5,7 @@ import { BaseLogPanelScene } from "./BaseLogPanelScene";
 
 export class LeaderboardScene extends BaseLogPanelScene {
     private levelInfoApi: LevelInfoImpl;
+    private recordTxtArray: Phaser.GameObjects.Text[] = [];
 
     constructor() {
         super('LeaderboardScene');
@@ -18,8 +19,8 @@ export class LeaderboardScene extends BaseLogPanelScene {
         this.add.rectangle(512, 384, 1024, 768, 0x000000, 80);
 
         var backToMain = this.make.text({
-            x: 10,
-            y: 500,
+            x: 100,
+            y: 700,
             text: 'Back to Level',
             style: { font: 'bold 20px Arial', color: '#00ff00' }
         });
@@ -31,12 +32,36 @@ export class LeaderboardScene extends BaseLogPanelScene {
         this.scene.launch('LoadingScene');
         this.levelInfoApi.getLevelTop10(LogicController.getInstance().getCurrentLevel().type)
         .then((levels: Level[]) => {
-            this.showLog(JSON.stringify(levels));
+            this.updateRank(levels);
             this.scene.stop('LoadingScene');
         })
         .catch(error => {
             this.showLog(error);
             this.scene.stop('LoadingScene');
         })
+    }
+
+    private updateRank(levels: Level[]): void {
+        for(var index = 0; index < 10; index ++) {
+            var recordTxt = this.make.text({
+                x: 100,
+                y: 100 + (index * 50),
+                text: (index + 1) + '. ',
+                style: { font: 'bold 40px Arial', color: '#ffffff' }
+            });
+            recordTxt.setAlign('center');
+            this.recordTxtArray[index] = recordTxt;
+        }
+
+        for (var index = 0; index < levels.length; index ++) {
+            var level = levels[index];
+            this.recordTxtArray[index].setText((index + 1) + '. ' + level.userName);
+            this.make.text({
+                x: 300,
+                y: 100 + (index * 50),
+                text: level.points + '',
+                style: { font: 'bold 40px Arial', color: '#ffffff' }
+            });
+        }
     }
 }
